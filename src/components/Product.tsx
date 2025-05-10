81800df27cc1e26516ca42d5dcae35a0c2217ce5
Serhii Zhelobkov <meeelnik@gmail.com>
2025-05-10 22:15:09 +0300

    beginning coding

---
+++ src/components/Product.tsx
@@ -0,0 +1,15 @@
+interface ProductProps {
+    name: string;
+    imgUrl: string;
+    price: number;
+};
+
+export default function Product(props: ProductProps) {
+    return (
+        <div>
+            <h2>{props.name}</h2>
+            <img src={props.imgUrl} alt={props.name} width="320" />
+            <p>Price: {props.price} credits</p>
+        </div>
+    );
+}
